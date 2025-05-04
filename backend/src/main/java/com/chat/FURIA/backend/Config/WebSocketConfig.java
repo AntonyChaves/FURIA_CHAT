ecddb80ac79b01eade539d4ad52a1de2b5bce163
Antony Cauã Chaves <119748304+AntonyChaves@users.noreply.github.com>
2025-05-03 22:54:42 -0300

    Update WebSocketConfig.java

--- backend/src/main/java/com/chat/FURIA/backend/Config/WebSocketConfig.java
+++ backend/src/main/java/com/chat/FURIA/backend/Config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/furia-chat");
+        registry.addEndpoint("/furia-chat").setAllowedOrigins("*");
 
     }
 }
